92ed0681f240dcf8662f9ef2cabd562899964e38
Amira Burns <burnsal@rams.colostate.edu>
2022-05-27 13:56:09 -0400

    Update ref list

--- ref_list.R
+++ ref_list.R
@@ -1,13 +1,7 @@
 #' Reference List
 #'
-#' Ananth, C., & Kleinbaum, D. (1997) Regression Models for Ordinal Responses: A Review of Methods and Applications.
-#' International Journal of Epidemiology, 26(6), 1323-1333 .https://doi.org/10.1093/ije/26.6.1323
+#' Agresti, A (2013). Categorical Data Analysis (Third Edition). John Wiley & Sons.
 #'
 #' Chernyavskiy, P., Edmondson, E. F., Weil, M. M., & Little, M. P. (2017). High-energy particle beam and gamma radiation exposure, familial relatedness and cancer in mice.
 #' British journal of cancer, 117(1), 41–50. https://doi.org/10.1038/bjc.2017.141
 #'
-#' Crouchley, R. (1995). A Random-Effects Model for Ordered Categorical Data.
-#' Journal of the American Statistical Association, 90(430), 489–498. https://doi.org/10.2307/2291059
-#'
-#' Hedeker, D., & Gibbons, R. D. (1994). A Random-Effects Ordinal Regression Model for Multilevel Analysis.
-#' Biometrics, 50(4), 933–944. https://doi.org/10.2307/2533433
