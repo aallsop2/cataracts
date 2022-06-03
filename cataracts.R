b5513c5b23f74094b1078ec65f6e40874b763af8
Alyssa Allsop <allsop.alyssa@gmail.com>
2022-06-02 23:03:56 -0600

    This file is kind of messy. I organized most of what I did in the report rmd file

--- cataracts.R
+++ cataracts.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(lmerTest)
 library(broom.mixed)
 
-cataracts <- read_excel("GRSD.cataract.xlsx")
+cats <- read_excel("C:/Users/allso/Documents/Grad School/STAA 556 Statistical Consulting/GRSD.cataract.xlsx")
 cataracts$groups <- as.factor(cataracts$groups)
 cataracts$cataract_score <- cataracts$`Cataract Score`
 cataracts$coat_color <- cataracts$`coat color`
@@ -31,7 +31,7 @@ full_mod <- lmer(cataract_score ~ sex + weight + BCS +
                    age + groups + myeloid_leukemia + harderian_tumor +
                    pret_lymphoma + (1|family) + (1|coat_color), data = cataracts)
 summary(full_mod)
-
+qqnorm(resid(full_mod))
 
 # check model assumptions ------------
 
@@ -44,7 +44,43 @@ ggplot(diagd, aes(x = .fitted, y = .resid)) + geom_point() + geom_hline(yinterce
 #These do not look good. Need to play around with different models. Maybe do a transformation?
 
 
+# create column of 0s and 1s for response
+cataracts <- cataracts %>%
+  mutate(cataract_status = ifelse(cataract_score >= 2, 1, 0))
 
+# fit binary response model ------
+binary_mod <- glmer(cataract_status ~ sex + weight + BCS +
+                     age + groups + myeloid_leukemia + harderian_tumor +
+                     pret_lymphoma + coat_color + (1|family),
+                    family = binomial, data = cataracts)
+
+
+diagd <- augment(binary_mod)
+str(diagd)
+ggplot(diagd, aes(sample = .resid)) + stat_qq() + facet_grid(~groups)
+ggplot(diagd, aes(sample = .resid)) + stat_qq() + facet_grid(~coat_color)
+ggplot(diagd, aes(sample = .resid)) + stat_qq()
+ggplot(diagd, aes(x = .fitted, y = .resid)) + geom_point() + geom_hline(yintercept = 0)
+
+qqnorm(resid(binary_mod))
+
+binary_mod2 <- glmer(cataract_status ~ sex + weight + BCS +
+                      groups + myeloid_leukemia + harderian_tumor +
+                      pret_lymphoma + coat_color + (1|family),
+                    family = binomial, data = cataracts)
+summary(binary_mod2)
+qqnorm(resid(binary_mod2))
+
+
+binary_mod3 <- glmer(cataract_status ~ sex + weight +
+                       groups + (1|family),
+                     family = binomial, data = cataracts)
+summary(binary_mod3)
+qqnorm(resid(binary_mod3), main = "mod 3")
+
+sub_cat <-cataracts[,c(3,5,7)]
+pairs(sub_cat)
+plot(cataracts$family, cataracts$weight)
 
 
 
