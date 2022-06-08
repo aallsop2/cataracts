c85ecd70522c0f328c093c2fcdd9bd3d36e49f22
Amira Burns <burnsal@rams.colostate.edu>
2022-06-07 20:08:47 -0600

    draft Bayes logistic regression with hyperparameter for family. Still buggy but want to commit it

--- ref_list.R
+++ ref_list.R
@@ -5,3 +5,9 @@
 #' Chernyavskiy, P., Edmondson, E. F., Weil, M. M., & Little, M. P. (2017). High-energy particle beam and gamma radiation exposure, familial relatedness and cancer in mice.
 #' British journal of cancer, 117(1), 41–50. https://doi.org/10.1038/bjc.2017.141
 #'
+#' Roback, P., Legler, J. (2021). Beyond Multiple Linear Regression.
+#' Applied Generalized Linear Models and Multilevel Models in R (Second Edition). CRC Press.
+#' Online version accessed 06/07/2022: https://bookdown.org/roback/bookdown-BeyondMLR/
+#'
+#' Update this citation: http://www.columbia.edu/~cjd11/charles_dimaggio/DIRE/styled-4/styled-11/code-8/#bayesian-regression-modeling-with-bugs-or-jags
+#'
